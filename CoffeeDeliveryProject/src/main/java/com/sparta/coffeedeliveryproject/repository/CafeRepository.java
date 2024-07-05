4dc10ce87f85aba68985449e9e735600554e6be2
dlalwn <annie45671@gmail.com>
2024-07-05 16:01:39 +0900

    좋아요한 카페 목록 기능 추가

--- CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/CafeRepository.java
+++ CoffeeDeliveryProject/src/main/java/com/sparta/coffeedeliveryproject/repository/CafeRepository.java
@@ -1,10 +1,19 @@
 package com.sparta.coffeedeliveryproject.repository;
 
 import com.sparta.coffeedeliveryproject.entity.Cafe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
     Optional<Cafe> findByCafeName(String cafeName);
+
+    @Query("SELECT c "
+            + "FROM Cafe c "
+            + "JOIN CafeLike cl ON c.cafeId = cl.cafe.cafeId "
+            + "WHERE cl.user.userId = :userId ")
+    Page<Cafe> findLikeCafeByUserUserId(Long userId, Pageable pageable);
 }
